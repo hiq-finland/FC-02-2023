0c2d3d1487fc06a374e0d43b8e2305cc673a6263
rinnehenrik <hencca77@gmail.com>
2022-11-08 21:38:50 +0200

    final

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 
 import Counter from "./Counter";
-import UserReducer1 from "./UserReducerStart";
+import UserReducer1 from "./UseReducerEnd";
 
 function App() {
   const [count, setCount] = useState(0);
